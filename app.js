861b150de600c2940c7555e19cee00a45ae903cf
JMan <60026263+JMantis0@users.noreply.github.com>
2020-06-24 02:34:35 -0700

    arranged prompt objects and instanciations into individual functions.  Switched .then()s into object destructurings.  Solved simultaneous prompts with asycn await syntax.

--- app.js
+++ app.js
@@ -9,193 +9,140 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { create } = require("domain");
 
 let empArr = [];
 let addMore = true;
 
-//  Inquirer object that prompts whether to add another employee.
-let inqNewEmp = [
-	{
-		type: 'confirm',
-		name: 'addAnother',
-		message: 'Add another employee?'
-	}
+async function getTypeToAdd() {
+	let tprompt = [
+		{
+			type: 'list',
+			name: 'empType',
+			message: 'Select:',
+			choices: ['Engineer', 'Intern']
+		}
+	];
 
-];
+	let type = await inquirer.prompt(tprompt);
+	return type;
+}
 
-//  Inquirer object that prompts what type of employee to add.
-let inqEmpType = [
-	{
-		type: 'list',
-		name: 'empType',
-		message: 'Select:',
-		choices: ['Engineer', 'Intern']
-	}
-
-];
-
-let inqEngineerData = [
-	{
-		type: 'input',
-		name: 'name',
-		message: `Name: `
-	},
-	{
-		type: 'input',
-		name: 'id',
-		message: `Employee ID: `
-	},
-	{
-		type: 'input',
-		name: 'email',
-		message: `Email address: `
-	},
-	{
-		type: 'input',
-		name: 'gitHub',
-		message: 'GitHub: '
-	}
-];
-
-let inqInternData = [
-	{
-		type: 'input',
-		name: 'name',
-		message: `Name: `
-	},
-	{
-		type: 'input',
-		name: 'id',
-		message: `Employee ID: `
-	},
-	{
-		type: 'input',
-		name: 'email',
-		message: `Email address: `
-	},
-	{
-		type: 'input',
-		name: 'school',
-		message: 'School: '
-	}
-];
-
-
-
-let inqManager = [
-	{
-		type: 'input',
-		name: 'name',
-		message: 'Full name: '
-	},
-	{
-		type: 'input',
-		name: 'id',
-		message: `Employee ID: `
-	},
-	{
-		type: 'input',
-		name: 'email',
-		message: `Email address: `
-	},
-	{
-		type: 'input',
-		name: 'officeNum',
-		message: 'Office telephone number: '	
-	}
-
-];
-
-console.log('Enter the team Manager\'s information: ');
-
-//  FIRST inquirer prompt adds for manager.
-inquirer.prompt(inqManager)
-.then(answer => {
-	empArr.push(new Manager(answer.name, answer.id, answer.email, answer.officeNum));
-	//  SECOND inquirer prompt gets type for next employee
-	while(addMore) {
-		console.log(`What type of employee would you like to add to ${empArr[0].name}'s team?`)
-		inquirer.prompt(inqEmpType).then(answer2 => {
-		
-				//  Switch pushes inquiry object to array according to type;
-			let promptType;
-			switch(answer2.empType) {
-			case "Engineer":
-				promptType = inqEngineerData;
-				break;
-			case "Intern":
-				promptType = inqInternData;
-				break;
-			}
-			//  THIRD inquirer prompt collects teammate's information and adds it to empArr
-			console.log(`Enter the ${answer2.empType}'s information: `)
-			inquirer.prompt(promptType)
-			.then(answer3 => {
-				console.log(answer3);
-				switch(answer2.empType) {
-					case "Engineer":
-						empArr.push(new Engineer(answer3.name, answer3.id, answer3.email, answer3.gitHub));
-						break;
-					case "Intern":
-						empArr.push(new Intern(answer3.name, answer3.id, answer3.email, answer3.school));
-						break;
-				}
-				console.log(empArr);
-				// FOURTH inquirer prompt asks whether to add more employee.
-				inquirer.prompt(inqNewEmp)
-				.then(answer4 => {
-					addMore = answer4.addAnother;
-				})
-				.catch(error => {
-					if (error) {
-						console.log(error);
-					}
-				});
-			})
-			.catch(error => {
-				if (error) {
-					console.log(error);
-				}
-			});;
-		})
-		.catch(error => {
-			if (error) {
-				console.log(error);
-			}
-		});;
-	}
-}).catch(error => {
-	if (error) {
-		console.log(error);
-	}
-});
-
-function inquiryFour() {
+async function addAnother() {
+	let aprompt = [
+		{
+			type: 'confirm',
+			name: 'addAnother',
+			message: 'Add another employee?'
+		}
+	];
+	let oneMore = await inquirer.prompt(aprompt);
+	console.log(oneMore);
+	return oneMore;
 
 }
 
+async function createEngineer() {
+	let eprompt = [
+		{
+			type: 'input',
+			name: 'name',
+			message: `Name: `
+		},
+		{
+			type: 'input',
+			name: 'id',
+			message: `Employee ID: `
+		},
+		{
+			type: 'input',
+			name: 'email',
+			message: `Email address: `
+		},
+		{
+			type: 'input',
+			name: 'gitHub',
+			message: 'GitHub: '
+		}
+	];
 
-//  Inquirer object that prompts for employee information
+	console.log("Enter your Engineer's information");
+	let {name, id, email, gitHub} = await inquirer.prompt(eprompt);
+	empArr.push(new Engineer(name, id, email, gitHub));
+	console.log('Engineer added.');
+	console.log(empArr);
 
-function askForAddEmployee() {
-	inquirer.prompt(inqNewEmp)
-	.then(answer => {
-		console.log(answer.addAnother)
-		if(answer.addAnother) {
-			
-		}
-		else {
-			addMore = false;
-			console.log("No more employees");
-		}
-	})
-	.catch(error => {
-		if (error) {
-			console.log(error);
-		}
-	});
 }
 
+async function createIntern() {
+	let iprompt = [
+		{
+			type: 'input',
+			name: 'name',
+			message: `Name: `
+		},
+		{
+			type: 'input',
+			name: 'id',
+			message: `Employee ID: `
+		},
+		{
+			type: 'input',
+			name: 'email',
+			message: `Email address: `
+		},
+		{
+			type: 'input',
+			name: 'school',
+			message: 'School: '
+		}
+	];
+
+	console.log("Enter your Intern's information");
+	let {name, id, email, school} = await inquirer.prompt(iprompt);
+	empArr.push(new Intern(name, id, email, school));
+	console.log('Intern added.')
+	console.log(empArr);
+}
+
+async function createManager() {
+	let mprompt = [
+		{
+			type: 'input',
+			name: 'name',
+			message: 'Full name: '
+		},
+		{
+			type: 'input',
+			name: 'id',
+			message: `Employee ID: `
+		},
+		{
+			type: 'input',
+			name: 'email',
+			message: `Email address: `
+		},
+		{
+			type: 'input',
+			name: 'officeNum',
+			message: 'Office telephone number: '	
+		}
+	];
+
+	console.log("Enter the team Manager's information")
+	let {name, id, email, officeNum} = await inquirer.prompt(mprompt);
+	empArr.push(new Manager(name, id, email, officeNum));
+	console.log('Manager added');
+	console.log(empArr);
+}
+
+async function init() {
+	await createManager();
+	await createEngineer();
+	await createIntern();
+}
+
+init();
 
 
 
