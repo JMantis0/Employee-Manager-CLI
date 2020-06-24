9f2b7b5625faf6f7ef6e70afa7ddade2c3de8f7b
JMan <60026263+JMantis0@users.noreply.github.com>
2020-06-24 12:26:50 -0700

    Begin aesthetic augmentation via chalk npm

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const chalk = require("chalk");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -14,6 +15,7 @@ const { get } = require("https");
 
 let empArr = [];
 let addMore = true;
+console.log(OUTPUT_DIR, outputPath)
 
 async function getTypeToAdd() {
 	let tprompt = [
@@ -21,7 +23,7 @@ async function getTypeToAdd() {
 			type: 'list',
 			name: 'empType',
 			message: 'Select:',
-			choices: ['Engineer', 'Intern']
+			choices: [chalk.yellow('Engineer'), chalk.green('Intern')]
 		}
 	];
 
@@ -81,26 +83,26 @@ async function createIntern() {
 		{
 			type: 'input',
 			name: 'name',
-			message: `Name: `
+			message: chalk.green(`Name: `)
 		},
 		{
 			type: 'input',
 			name: 'id',
-			message: `Employee ID: `
+			message: chalk.green(`Employee ID: `)
 		},
 		{
 			type: 'input',
 			name: 'email',
-			message: `Email address: `
+			message: chalk.green(`Email address: `)
 		},
 		{
 			type: 'input',
 			name: 'school',
-			message: 'School: '
+			message: chalk.green('School: ')
 		}
 	];
 
-	console.log("Enter your Intern's information");
+	console.log(chalk.bold(`Enter your ${chalk.green(`Intern's`)} information`));
 	let {name, id, email, school} = await inquirer.prompt(iprompt);
 	empArr.push(new Intern(name, id, email, school));
 	console.log('Intern added.')
@@ -112,26 +114,26 @@ async function createManager() {
 		{
 			type: 'input',
 			name: 'name',
-			message: 'Full name: '
+			message: chalk.red('Full name: ')
 		},
 		{
 			type: 'input',
 			name: 'id',
-			message: `Employee ID: `
+			message: chalk.red(`Employee ID: `)
 		},
 		{
 			type: 'input',
 			name: 'email',
-			message: `Email address: `
+			message: chalk.red(`Email address: `)
 		},
 		{
 			type: 'input',
 			name: 'officeNum',
-			message: 'Office telephone number: '	
+			message: chalk.red('Office telephone number: ')
 		}
 	];
 
-	console.log("Enter the team Manager's information")
+	console.log(chalk.bold(`Enter the team ${chalk.red(`Manager's`)} information"`))
 	let {name, id, email, officeNum} = await inquirer.prompt(mprompt);
 	empArr.push(new Manager(name, id, email, officeNum));
 	console.log('Manager added');
@@ -143,40 +145,15 @@ async function init() {
 
 	while(await addAnother()) {
 		switch(await getTypeToAdd()) {
-			case "Engineer":
+			case chalk.yellow('Engineer'):
 				await createEngineer();
 				break;
-			case "Intern":
+			case chalk.green("Intern"):
 				await createIntern();
 				break;
 		}
 	}
-
+	fs.writeFileSync(outputPath, render(empArr), "utf-8");
 }
 
 init();
-
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
